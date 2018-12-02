b2893085382a55ae63dcc561c88e8b846b46376e
Andrew Sleigh <andrewsleigh@akauk.com>
2018-12-02 19:16:39 +0000

    projects page added

--- server.js
+++ server.js
@@ -49,6 +49,12 @@ app.get('/about', (req, res) => {
     });
 })
 
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Projects uh',
+    })
+})
+
 app.get('/bad', (req, res) => {
     res.send({
         errorMessage: 'failed handling request'
